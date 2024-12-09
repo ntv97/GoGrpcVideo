f966efd44aad4081081e31862c8af96212d38525
nhivo <nhivo@ubuntu.myguest.virtualbox.org>
2024-12-08 18:55:08 -0800

    .

---
+++ server/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+ "fmt"
+ //"context"
+ uploadpb "github.com/GoGrpcVideo/proto"
+ "github.com/GoGrpcVideo/pkg/app"
+ "google.golang.org/grpc"
+ "log"
+ //"net"
+ "bytes"
+ "os"
+ "path/filepath"
+ "io"
+)
+
+type FileServiceServer struct {
+	uploadpb.UnimplementedFileServiceServer
+}
+
+type File struct {
+	FilePath   string
+	buffer     *bytes.Buffer
+	OutputFile *os.File
+}
+
+func NewFile() *File {
+	return &File{
+		buffer: &bytes.Buffer{},
+	}
+}
+
+func (f *File) SetFile(fileName, path string) error {
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.FilePath = filepath.Join(path, fileName)
+	file, err := os.Create(f.FilePath)
+	if err != nil {
+		return err
+	}
+	f.OutputFile = file
+	return nil
+}
+
+func (f *File) Write(chunk []byte) error {
+	if f.OutputFile == nil {
+		return nil
+	}
+	_, err := f.OutputFile.Write(chunk)
+	return err
+}
+
+func (f *File) Close() error {
+	return f.OutputFile.Close()
+}
+
+func (g *FileServiceServer) Upload(stream uploadpb.FileService_UploadServer) error {
+	file := NewFile()
+	var fileSize uint32
+	fileSize = 0
+	defer func() {
+		if err := file.OutputFile.Close(); err != nil {
+			fmt.Println("Error 0")
+			return
+		}
+	}()
+	for {
+		req, err := stream.Recv()
+		if file.FilePath == "" {
+			file.SetFile(req.GetFileName(), "./videosgrpc")
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error 1")
+			return nil
+			
+		}
+		chunk := req.GetChunk()
+		fileSize += uint32(len(chunk))
+		if err := file.Write(chunk); err != nil {
+			fmt.Println("Error 2")
+			return nil
+		}
+	}
+
+	fileName := filepath.Base(file.FilePath)
+	return stream.SendAndClose(&uploadpb.FileUploadResponse{FileName: fileName, Size: fileSize})
+}
+
+func main() {
+	cfg := app.DefaultConfig()
+	err := cfg.ReadFile("config.json")
+        if err != nil && !os.IsNotExist(err) {
+                log.Fatal(err)
+        }
+        a, err := app.NewApp(cfg)
+        if err != nil {
+                log.Fatal(err)
+        }
+        addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
+        log.Printf("Local server: http://%s", addr)
+        err = a.Run()
+        if err != nil {
+                log.Fatal(err)
+        }
+	/*lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		fmt.Println("failed to listen on port 50051: %v", err)
+		return
+	}
+	fmt.Println("Listening on :50051") */
+	g := grpc.NewServer()
+	uploadpb.RegisterFileServiceServer(g, &FileServiceServer{})
+	if err := g.Serve(a.Listener); err != nil {
+		fmt.Println("Serve error")
+		return
+	}
+
+}
